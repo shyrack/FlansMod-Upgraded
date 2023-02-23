98081b17537ac08e2f65abf54f2da4e0de1198f4
Florian <CodeBucketCore@gmail.com>
2023-02-23 21:54:14 +0100

    fixed imports of BlockUtil class

--- src/main/java/com/shyrack/flansmodupgraded/common/util/BlockUtil.java
+++ src/main/java/com/shyrack/flansmodupgraded/common/util/BlockUtil.java
@@ -1,40 +1,30 @@
 package com.shyrack.flansmodupgraded.common.util;
 
-import java.util.UUID;
-
 import com.mojang.authlib.GameProfile;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.WorldServer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.FakePlayer;
-import net.minecraftforge.event.world.BlockEvent.BreakEvent;
+import net.minecraftforge.event.level.BlockEvent;
 
-public class BlockUtil
-{
-	public static boolean destroyBlock(WorldServer world, BlockPos pos, Entity entity, boolean dropBlock)
-	{
-		EntityPlayer player;
-		if(entity instanceof EntityPlayer)
-		{
-			player = (EntityPlayer)entity;
-		}
-		else
-		{
-			player = new FakePlayer(world, new GameProfile(UUID.randomUUID(), "fakePlayer"));
-		}
-		
-		BreakEvent breakEvent = new BreakEvent(world, pos, world.getBlockState(pos), player);
-		MinecraftForge.EVENT_BUS.post(breakEvent);
-		
-		if(breakEvent.isCanceled())
-		{
-			return false;
-		}
-		
-		world.destroyBlock(pos, dropBlock);
-		
-		return true;
-	}
+import java.util.UUID;
+
+public class BlockUtil {
+
+    public static boolean destroyBlock(ServerLevel level, BlockPos pos, Entity entity, boolean dropBlock) {
+        Player player = entity instanceof Player ? (Player) entity : new FakePlayer(level, new GameProfile(UUID.randomUUID(), "fakePlayer"));
+        BlockEvent.BreakEvent breakEvent = new BlockEvent.BreakEvent(level, pos, level.getBlockState(pos), player);
+        MinecraftForge.EVENT_BUS.post(breakEvent);
+
+        if (breakEvent.isCanceled()) {
+            return false;
+        }
+
+        level.destroyBlock(pos, dropBlock);
+
+        return true;
+    }
+
 }
